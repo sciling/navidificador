4e85dea395d1c6a12a228627864690d7688f137f
Vicent Alabau <valabau@sciling.com>
2022-12-14 19:50:12 +0100

    Add poem endpoint

--- src/bin/server.py
+++ src/bin/server.py
@@ -1,3 +1,4 @@
+# pylint: disable=unused-import
 import os
 import io
 import json
@@ -5,15 +6,18 @@ import base64
 from typing import Union, Optional
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request, File, UploadFile
+from fastapi import FastAPI, HTTPException, Request, File, UploadFile
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import requests
 import magic
 from PIL import Image
+import openai
 
 # from navidificador import logging
 
 load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 app = FastAPI()
 
 
@@ -47,6 +51,13 @@ def base64_to_image(image):
     return base64.b64decode(image)
 
 
+def create_full_mask(image):
+    with Image.open(io.BytesIO(image)) as img:
+        image_bytes = io.BytesIO()
+        Image.new('RGB', (img.size)).save(image_bytes, 'PNG')
+        return image_bytes.getvalue()
+
+
 def get_mask(image):
     mime = magic.from_buffer(image, mime=True)
     print(f"image: {mime} {image[:100]}")
@@ -54,16 +65,15 @@ def get_mask(image):
 
     try:
         mask = next((base64_to_image(im['mask']) for im in masks if im['label'] == 'person'), None)
-    except Exception as e:
-        print(e)
+    except Exception as ex:
+        print(ex)
         mask = None
 
     if mask is None:
-        print('WARNING: no mask found!')
-        with Image.open(io.BytesIO(image)) as im:
-            image_bytes = io.BytesIO()
-            Image.new('RGB', (im.size)).save(image_bytes, 'PNG')
-            mask = image_bytes.getvalue()
+        mask = create_full_mask(image)
+
+    if mask is None:
+        raise HTTPException(status_code=500, detail="Could not generate a mask image")
 
     with open('mask.png', 'wb') as file:
         file.write(mask)
@@ -82,6 +92,17 @@ def get_inpaint(image, mask):
     return output
 
 
+def valid_image_format(image):
+    return True
+
+
+def validation_error(message, data=None):
+    return JSONResponse(
+        status_code=422,
+        content={"message": message, "data": data},
+    )
+
+
 @app.get("/")
 async def root():
     return {"message": "Soy el Navidificador!"}
@@ -89,14 +110,16 @@ async def root():
 
 class ImageModel(BaseModel):
     image: str
-    prompt: Optional[str] = ''
+    prompt: Optional[str] = None
 
 
 def process_image(image):
     image = base64_to_image(image.image)
     mask = get_mask(image)
     images = [image_to_base64(mask)]
-    return {"images": images}
+    return {
+        "images": images,
+    }
 
 
 @app.post("/image")
@@ -108,6 +131,59 @@ async def process_image_json(image: ImageModel):
 async def process_image_file(image_file: UploadFile):
     image = ImageModel(image=image_to_base64(image_file))
     return process_image(image)
+
+
+class PoemModel(BaseModel):
+    name: str
+    description: str
+    language: Optional[str] = 'es'
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Cristóbal Colón",
+                "description": "Cristóbal Colón (Cristoforo Colombo, en italiano, o Christophorus Columbus, en latín; de orígenes discutidos, los expertos se inclinan por Génova, República de Génovan. 1​3​4​ donde pudo haber nacido el 31 de octubre de 14515​ y se sabe que murió en Valladolid el 20 de mayo de 1506) fue un navegante, cartógrafo, almirante, virrey y gobernador general de las Indias Occidentales al servicio de la Corona de Castilla. Realizó el llamado descubrimiento de América el 12 de octubre de 1492, al llegar a la isla de Guanahani, en las Bahamas.",
+                "language": 'es',
+            }
+        }
+
+
+@app.post("/poem")
+async def create_poem(poem: PoemModel):
+    """ Produces a Christmas poem addressed to a specific person.
+        Description should have some details of that person so that the poem can be personalized.
+    """
+
+    if poem.language == 'en':
+        prompt = f"""
+        Generate a Christmas story in the form of a Shakespeare sonnet with a person named {poem.name} and the following CV.
+        Christmas is very important.
+
+        {poem.description}
+        """
+    else:
+        prompt = f"""
+        Genera un cuento de Navidad en forma de soneto de Lope de Vega con una persona llamada {poem.name} y el siguiente currículum.
+        La Navidad es muy importante.
+
+        {poem.description}
+        """
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        best_of=1
+    )
+
+    text = response['choices'][0]['text']
+    return {
+        'poem': text,
+    }
 
 
 def start():
