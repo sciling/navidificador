1c11b63130cba75dc4712f48fc4a9805e5e6286e
Vicent Alabau <valabau@sciling.com>
2022-12-16 11:08:07 +0100

    Improve image campaign support

--- src/bin/server.py
+++ src/bin/server.py
@@ -1,9 +1,11 @@
+# pylint: disable=too-few-public-methods
 import base64
 import io
 import os
 import re
 import sys
 
+from inspect import cleandoc
 from typing import List
 from typing import Optional
 
@@ -19,6 +21,7 @@ from PIL import Image
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
 
 from navidificador import logging  # pylint: disable=unused-import # noqa: F401
+from navidificador.fastapi import AppException
 from navidificador.fastapi import UserException
 from navidificador.fastapi import app
 from navidificador.fastapi import responses
@@ -30,6 +33,62 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 logger.debug("Python %s", sys.version.replace("\n", " "))
+
+
+def clean_spaces(text):
+    text = re.sub(r"^\s*", "", text)
+    text = re.sub(r"\s*$", "", text)
+    text = re.sub(r"\s+", " ", text)
+    return text
+
+
+campaigns = {
+    "navidad": {
+        "inpaint": {
+            "prompt": clean_spaces(
+                """
+                snowy christmas card, thomas kinkade, snowing, 8k ,Wide angle cinematography of toonces,
+                a christmas eve photorealistic painting on the wall, home, interior, octane render,
+                deviantart, cinematic, key art, hyperrealism, canon eos c 3 0 0, ƒ 1. 8, 3 5 mm,
+                medium - format print
+            """
+            ),
+            "negative_prompt": clean_spaces(
+                """
+                duplicate, fog, darkness, grain, disfigured, kitsch, ugly, oversaturated, grain,
+                low-res, Deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation,
+                mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs,
+                disconnected limbs, malformed hands, blur, out of focus, long neck, long body, ugly,
+                disgusting, poorly drawn, childish, mutilated, mangled, old, surreal, bad artist
+            """
+            ),
+            "guidance_scale": 7.5,
+            "num_samples": 4,
+            "strength": 0.4,
+            "inference_steps": 75,
+        },
+        "poem_prompt": {
+            "en": cleandoc(
+                """
+                Generate a Christmas story in the form of a Shakespeare sonnet with a person named and the following CV.
+                Christmas is very important.
+
+                {poem.description}
+            """
+            ),
+            "es": cleandoc(
+                """
+                Genera un cuento de Navidad en forma de soneto de Lope de Vega con una persona llamada y el siguiente currículum.
+                El poema tiene que tener una rima consonante.
+                Los versos tienen que rimar de dos en dos y está prohibida la rima asonante.
+                La Navidad es muy importante.
+
+                {poem.description}
+            """
+            ),
+        },
+    },
+}
 
 
 @profile(desc=0)
@@ -57,13 +116,6 @@ def api(service, data, **kwargs):
     response = requests.post(os.getenv(service + "_HUGGINGFACE_ENDPOINT"), headers=headers, **params)
 
     return response.json()
-
-
-def clean_spaces(text):
-    text = re.sub(r"^\s*", "", text)
-    text = re.sub(r"\s*$", "", text)
-    text = re.sub(r"\s+", " ", text)
-    return text
 
 
 def read_b64(filename, ensure_ascii=False):
@@ -111,12 +163,15 @@ def get_mask(image):
     return mask
 
 
-def get_inpaint(image, mask):
+def get_inpaint(image, mask, campaign):
+    if "inpaint" not in campaigns[campaign]:
+        raise AppException(msg=f"Campaign '{campaign}' does not have inpaint configuration.")
+
     data = {
         "inputs": "inpaint",
         "image": image_to_base64(image, ensure_ascii=True),
         "mask": image_to_base64(mask, ensure_ascii=True),
-        "config": campaigns["navidad"]["inpaint"],
+        "config": campaigns[campaign]["inpaint"],
     }
     output = api("inpaint", data)
     return output
@@ -163,11 +218,13 @@ async def stats():
 class ImageModel(BaseModel):
     image: str
     prompt: Optional[str] = None
+    campaign: str = "navidad"
 
     class Config:
         schema_extra = {
             "example": {
                 "image": read_b64("resources/vader.jpg"),
+                "campaign": "navidad",
                 "prompt": None,
             }
         }
@@ -185,20 +242,23 @@ class ImageResponseModel(BaseModel):
 
 
 def process_image(image):
-    image = base64_to_image(image.image)
+    if image.campaign not in campaigns:
+        raise UserException(msg=f"Invalid campaign '{image.campaign}'", target="campaign")
 
-    validate_image_format(image, "input image")
-    mask = get_mask(image)
+    img = base64_to_image(image.image)
 
-    validate_image_format(image, "mask image")
-    images = get_inpaint(image, mask)
+    validate_image_format(img, "input image")
+    mask = get_mask(img)
+
+    validate_image_format(img, "mask image")
+    images = get_inpaint(img, mask, image.campaign)
 
     for index, item in enumerate(images):
         validate_image_format(base64_to_image(item["image"]), f"result image[{index}]")
 
         if os.path.isdir("tmp"):
-            image = Image.open(io.BytesIO(base64.b64decode(item["image"])))
-            image.save(f"tmp/image{index}.jpg")
+            tmp_img = Image.open(io.BytesIO(base64.b64decode(item["image"])))
+            tmp_img.save(f"tmp/image{index}.jpg")
 
     return images
 
@@ -247,49 +307,6 @@ class PoemResponseModel(BaseModel):
                 "text": "the text of the poem",
             }
         }
-
-
-campaigns = {
-    "navidad": {
-        "inpaint": {
-            "prompt": clean_spaces(
-                """
-                snowy christmas card, thomas kinkade, snowing, 8k ,Wide angle cinematography of toonces,
-                a christmas eve photorealistic painting on the wall, home, interior, octane render,
-                deviantart, cinematic, key art, hyperrealism, canon eos c 3 0 0, ƒ 1. 8, 3 5 mm,
-                medium - format print
-            """
-            ),
-            "negative_prompt": clean_spaces(
-                """
-                duplicate, fog, darkness, grain, disfigured, kitsch, ugly, oversaturated, grain,
-                low-res, Deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation,
-                mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs,
-                disconnected limbs, malformed hands, blur, out of focus, long neck, long body, ugly,
-                disgusting, poorly drawn, childish, mutilated, mangled, old, surreal, bad artist
-            """
-            ),
-            "guidance_scale": 7.5,
-            "num_samples": 4,
-            "strength": 0.4,
-            "inference_steps": 75,
-        },
-        "poem_prompt": {
-            "en": """
-                Generate a Christmas story in the form of a Shakespeare sonnet with a person named and the following CV.
-                Christmas is very important.
-
-                {poem.description}
-            """,
-            "es": """
-                Genera un cuento de Navidad en forma de soneto de Lope de Vega con una persona llamada y el siguiente currículum.
-                La Navidad es muy importante.
-
-                {poem.description}
-            """,
-        },
-    },
-}
 
 
 @app.post("/poem", response_model=PoemResponseModel, responses=responses)
