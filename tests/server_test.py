49e57d3171b9b011b19a274dc5ac243fec5cd0bc
Vicent Alabau <valabau@sciling.com>
2022-12-22 17:04:31 +0100

    Improve tests

--- tests/server_test.py
+++ tests/server_test.py
@@ -5,9 +5,20 @@ from bin.server import limit_poem
 
 class TestServer(unittest.TestCase):
     def test_limit_poem(self):
-        text = "\n1\n\n2\n\n3\n4\n5\n6\n\n7\n8\n9\n1\n"
-        res = limit_poem(text)
-        self.assertEqual(res, "3\n4\n5\n6\n\n7\n8\n9\n1")
+        tests = [
+            (
+                "\n1\n\n2\n\n3\n4\n5\n6\n\n7\n8\n9\n1\n\nb3\nb4\nb5\nb6\n\nb7\nb8\nb9\nb1\n\nc3\nc4\nc5\nc6\n\nc7\nc8\nc9\nc1\n",
+                "3\n4\n5\n6\n\n7\n8\n9\n1\n\nb3\nb4\nb5\nb6\n\nb7\nb8\nb9\nb1",
+            ),
+            (
+                "\n1\n\n2\n\n3\n4\n5\n6",
+                "3\n4\n5\n6",
+            ),
+        ]
+
+        for text, ref in tests:
+            res = limit_poem(text)
+            self.assertEqual(res, ref)
 
 
 if __name__ == "__main__":
